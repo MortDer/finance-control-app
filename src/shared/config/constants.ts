e04ce6df51ecc988ac91518d0904322064fe96e8
MortDer <mortder@hotmail.com>
2026-03-10 15:42:05 +0300

    fix: fix base url

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -4,5 +4,5 @@ export const THEME_STORAGE_KEY = 'app-theme'
 export const LANGUAGE_STORAGE_KEY = 'app-language'
 export const AUTH_TOKEN_STORAGE_KEY = 'auth-token'
 
-export const API_BASE_URL = 'http://19429ba06ff2.vps.myjino.ru/api'
+export const API_BASE_URL = 'https://19429ba06ff2.vps.myjino.ru/api'
 export const COMMAND_ID = 'mortder'
